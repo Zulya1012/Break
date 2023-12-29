ca00e16b3d74e75a10a0a7569f4b29da026a9502
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-29 22:19:03 +0400

    Update loops.java

--- loops.java
+++ loops.java
@@ -1,12 +1,12 @@
-public class loops{
-public static void main(String[] arg){
-for (int i = 1; i <= 3; i++) {
-System.out.println(i);
-for (int a = 1; a <= 10; a++) {
-System.out.println(a);
-if (a == 2)
-break;
-}
- }
-}
+public class loops {
+  public static void main(String[] arg) {
+    for (int i = 1; i <= 3; i++) {
+      System.out.println(i);
+      for (int a = 1; a <= 10; a++) {
+        System.out.println(a);
+        if (a == 2)
+          break;
+      }
+    }
+  }
 }
